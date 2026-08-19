33abc9ac03228242c469e16887eed99007192d95
y3Enzo <enzochalimpus@outlook.com>
2026-08-18 22:10:07 -0300

    feat: reorganiza arquivos do banco, integra .env no banco.py e adiciona requirements.txt

--- dados/banco.py
+++ dados/banco.py
@@ -1,14 +1,21 @@
+import os
 import sqlite3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CAMINHO_BANCO_DB = os.getenv('CAMINHO_BANCO_DB')
+CAMINHO_SCHEMA_SQL = os.getenv('CAMINHO_SCHEMA_SQL')
 
 def obter_conexao():
-    conexao = sqlite3.connect('banco.db')
+    conexao = sqlite3.connect(CAMINHO_BANCO_DB)
     conexao.row_factory = sqlite3.Row
     cursor = conexao.cursor()
     return (conexao, cursor)
 
 def criar_banco():
     conexao, cursor = obter_conexao()
-    with open('schema.sql', 'r', encoding='utf-8') as schema:
+    with open(CAMINHO_SCHEMA_SQL, 'r', encoding='utf-8') as schema:
         schema_sql = schema.read()
 
     cursor.executescript(schema_sql)
